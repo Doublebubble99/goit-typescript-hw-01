5ba632acb384d0e759f76caebb101334f9b77679
Artemon Chik <zagrousaspear@gmail.com>
2023-12-21 17:06:28 +0100

    typescript-01

---
+++ dist/test.js
@@ -0,0 +1,6 @@
+"use strict";
+function typeCompare(firstArg, secondArg) {
+    return firstArg + secondArg;
+}
+console.log(typeCompare("Hello", "user"));
+//# sourceMappingURL=test.js.map
---
+++ src/test.ts
@@ -0,0 +1,4 @@
+function typeCompare(firstArg: string, secondArg: string): string {
+  return firstArg + secondArg;
+}
+console.log(typeCompare("Hello", "user"));
